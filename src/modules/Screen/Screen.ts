7e7b9f99bcefb47fc73ff898bfd08a5b69aadc91
Maxim Semenishch <3blik230@gmail.com>
2021-06-17 18:34:44 +0300

    Initial commit

---
+++ src/modules/Screen/Screen.ts
@@ -0,0 +1,51 @@
+export type ScreenMap = number[][];
+
+interface IPalette {
+  0: string;
+  [key: number]: string;
+}
+
+const defaultPalette: IPalette = {
+  0: "#ddd",
+  1: "#666",
+  2: "red",
+  3: "blue",
+  4: "brown"
+};
+
+export class Screen {
+  constructor(
+    private ctx: CanvasRenderingContext2D,
+    private unitSize: number,
+    private pallete: IPalette = defaultPalette
+  ) {}
+
+  private drawUnit = (x: number, y: number, color = "#ccc") => {
+    this.ctx.strokeStyle = "white";
+    this.ctx.fillStyle = color;
+    this.ctx.lineWidth = 2;
+
+    this.ctx.fillRect(
+      x * this.unitSize,
+      y * this.unitSize,
+      this.unitSize - 1,
+      this.unitSize - 1
+    );
+    this.ctx.rect(
+      x * this.unitSize + 3,
+      y * this.unitSize + 3,
+      this.unitSize - 7,
+      this.unitSize - 7
+    );
+    this.ctx.stroke();
+  };
+
+  render = (screenMap: ScreenMap) => {
+    screenMap.forEach((row, rowIndex) => {
+      row.forEach((col, colIndex) => {
+        const color = this.pallete[col] || this.pallete[0];
+        this.drawUnit(colIndex, rowIndex, color);
+      });
+    });
+  };
+}
